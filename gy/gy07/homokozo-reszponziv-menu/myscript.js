8430db8bfb7e2540dab5bacd392d6381311374e6
Emese <8emi95@inf.elte.hu>
2019-03-16 18:12:48 +0100

    Initial commit

---
+++ gy/gy07/homokozo-reszponziv-menu/myscript.js
@@ -0,0 +1,4 @@
+function myFunction() //megkeresi menü osztályú elemeket, elsőt kiválasztja (1 iylen van), osztály listát bővíti/elveszi
+{
+	document.getElementsByClassName("menu")[0].classList.toggle("kibont");
+}
